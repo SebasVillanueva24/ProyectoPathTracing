c4ab636a5677977d0c441ff31c1b49aeba03467e
Sebastián Villanueva <elsebuchas@hotmail.com>
2020-06-22 18:39:14 -0600

    pinta imagen

--- Ray.py
+++ Ray.py
@@ -1,9 +1,12 @@
 from Point import *
 
 class Ray:
+
     pos = Point(0,0)
     source = Point(0,0)
+    dir = source - pos
 
     def __init__(self, pos, source):
         self.pos = pos
         self.source = source
+        self.dir = source - pos
